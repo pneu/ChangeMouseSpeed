3f3d46061401e89902b55d7e7dcd4a3ad50aa91a
pneu <pneumaster@gmail.com>
2016-04-17 01:01:50 +0900

    initial commit

---
+++ ChangeMouseSpeed/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChangeMouseSpeed
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    /// <see cref="http://program.station.ez-net.jp/special/vcs/mouse/speed.asp"/>
+    public partial class MainWindow : Window
+    {
+        int DefaultMaximumSpeed { get; } = MouseSpeed.Max;
+        int RestrictedMaximumSpeed;
+
+        public MainWindow() {
+            InitializeComponent();
+            InitSpeed();
+        }
+
+        void InitSpeed() {
+            var current = slider.Value;
+            RestrictedMaximumSpeed = (int)current;
+            try {
+                current = MouseSpeed.Get();
+                slider.Value = current;
+                RestrictedMaximumSpeed = (int)current;
+                slider.Maximum = current;
+            }
+            catch { /* nop */ }
+        }
+
+        void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            try {
+                MouseSpeed.Set((int)slider.Value);
+            }
+            catch { /* nop */ }
+        }
+
+        void checkBox_Checked(object sender, RoutedEventArgs e) {
+            /* slider.Value set to Maximum if above RestrictedMaximumSpeed automatically  */
+            slider.Maximum = RestrictedMaximumSpeed;
+        }
+
+        void checkBox_Unchecked(object sender, RoutedEventArgs e) {
+            slider.Maximum = DefaultMaximumSpeed;
+        }
+    }
+}
---
+++ ChangeMouseSpeed/MouseSpeed.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeMouseSpeed
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    /// <see cref="http://program.station.ez-net.jp/special/vcs/mouse/speed.asp"/>
+    public static class MouseSpeed
+    {
+        [System.Runtime.InteropServices.DllImport("user32", SetLastError = true)]
+        static extern bool SystemParametersInfo(uint uiAction, uint uiParam, IntPtr pvParam, uint fWinIni);
+
+        public static int Min = 0;
+        public static int Max = 20;
+        const uint SPI_GETMOUSESPEED = 0x70;
+        const uint SPI_SETMOUSESPEED = 0x71;
+
+        public static void Set(int speed) {
+            if (!SystemParametersInfo(SPI_SETMOUSESPEED, 0, new IntPtr(speed), 0)) {
+                throw new Exception("マウスカーソルの速度を設定できませんでした。");
+            }
+        }
+
+        unsafe public static int Get() {
+            int result;
+
+            if (!SystemParametersInfo(SPI_GETMOUSESPEED, 0, new IntPtr((void*)&result), 0)) {
+                throw new Exception("マウスカーソルの速度を取得できませんでした。");
+            }
+
+            return result;
+        }
+    }
+}
